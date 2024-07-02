a21f784c6b999b04c5379772036e580a85a0923a
Abdulateef Ayomide Alakija <133767168+ayomiiee@users.noreply.github.com>
2024-07-02 12:11:59 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function updateTime() {
+    const now = new Date();
+    const utcTime = now.toUTCString().split(' ')[4];
+    const dayOfWeek = now.toLocaleString('en-US', { weekday: 'long' });
+
+    document.querySelector('[data-testid="currentTimeUTC"]').textContent = utcTime;
+    document.querySelector('[data-testid="currentDay"]').textContent = dayOfWeek;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
